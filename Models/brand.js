bd11b2e11d808cf7821c15c8ce2e4df461408dee
thebigbeanest <tannergilliambusiness@gmail.com>
2024-05-29 09:57:08 -0400

    fleshed out brand.js, index.js, and product.js.

---
+++ Models/brand.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const BrandSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    url: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Brand', BrandSchema);
