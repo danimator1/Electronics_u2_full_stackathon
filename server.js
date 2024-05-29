ed0d459498d450a65b51af00538a77e6115fb8f5
DRaymond <danimator1@gmail.com>
2024-05-29 18:52:48 -0400

    update controllers

--- server.js
+++ server.js
@@ -48,6 +48,56 @@ app.get('/brand/:id', async (req, res) =>{
   }
 })
 
+app.delete('/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.deleteOne({ _id: id });
+    if (deletedProduct.deletedCount === 0) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.delete('/brand/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBrand = await Brand.deleteOne({ _id: id });
+    if (deletedBrand.deletedCount === 0) {
+      return res.status(404).json({ error: 'Brand not found' });
+    }
+    res.json({ message: 'Brand deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// POST /product
+app.post('/product', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).json({ message: 'Product created successfully', newProduct });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// POST /brand
+app.post('/brand', async (req, res) => {
+  try {
+    const newBrand = await Brand.create(req.body);
+    res.status(201).json({ message: 'Brand created successfully', newBrand });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
   })
